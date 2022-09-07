0600de3b0934ca3f65e6af09e2674044c141ebd5
masujima <masujima.ernar@gmail.com>
2022-09-07 20:34:16 +0600

    Setup UI

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Yernar Masujima on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BarcodeScannerView()
+        }
+    }
+}
