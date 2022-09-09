b1f789b01748725af3eec336ac4edbbb73051892
masujima <masujima.ernar@gmail.com>
2022-09-09 21:52:11 +0600

    Refactor code

---
+++ BarcodeScanner/Alert.swift
@@ -0,0 +1,21 @@
+//
+//  Alert.swift
+//  BarcodeScanner
+//
+//  Created by Yernar Masujima on 9/9/22.
+//
+
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: String
+    let message: String
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input", message: "Something is wrong with the camera. We are unable to capture the input.", dismissButton: .default(Text("OK")))
+    
+    static let invalidScanType = AlertItem(title: "Invalid Scan Type", message: "The value scanned is not valid. This app scans EAN-8 and EAN-13.", dismissButton: .default(Text("OK")))
+}
---
+++ BarcodeScanner/BarcodeScannerViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  BarcodeScannerViewModel.swift
+//  BarcodeScanner
+//
+//  Created by Yernar Masujima on 9/9/22.
+//
+
+import SwiftUI
+
+final class BarcodeScannerViewModel: ObservableObject {
+    @Published var scannedCode = ""
+    @Published var alertItem: AlertItem?
+    
+    var statusText: String {
+        scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
+    
+    var statusTextColor: Color {
+        scannedCode.isEmpty ? .red : .green
+    }
+}
